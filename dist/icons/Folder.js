f520afe6c67fb061bc23dfcacdd1018cccca69d9
Josh Kennedy <jshknndy@gmail.com>
2017-12-05 14:24:01 -0600

    Initial commit

---
+++ dist/icons/Folder.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Folder = props => (
+  <svg width="1em" height="1em" viewBox="0 0 1792 1792" {...props}>
+    <path d="M1728 608v704q0 92-66 158t-158 66H288q-92 0-158-66t-66-158V352q0-92 66-158t158-66h320q92 0 158 66t66 158v32h672q92 0 158 66t66 158z" />
+  </svg>
+);
+
+export default Folder;
---
+++ dist/icons/Plus.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Plus = props => (
+  <svg width="1em" height="1em" viewBox="0 0 1792 1792" {...props}>
+    <path d="M1600 736v192q0 40-28 68t-68 28h-416v416q0 40-28 68t-68 28H800q-40 0-68-28t-28-68v-416H288q-40 0-68-28t-28-68V736q0-40 28-68t68-28h416V224q0-40 28-68t68-28h192q40 0 68 28t28 68v416h416q40 0 68 28t28 68z" />
+  </svg>
+);
+
+export default Plus;
